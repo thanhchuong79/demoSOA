2f49ac71f5d689769176dbff61473c34e1fe80dd
thanhchuong79 <123515117+thanhchuong79@users.noreply.github.com>
2023-11-29 10:48:30 -0500

    ajoute des attributs

--- src/main/java/soa/LifeLeftService.java
+++ src/main/java/soa/LifeLeftService.java
@@ -1,4 +1,11 @@
 package soa;
 
 public class LifeLeftService {
+    private static final Integer ESPARANCE_VIE_HOMMES = 79;
+    private static final Integer ESPARANCE_VIE_FEMMES = 85;
+
+    String homme = "homme";
+    String femme = "femme";
+
+    Integer evDeReference = 0;
 }
